2e6f0cfcc6bb829aac10ab64759fb4694ab6dea6
Md Istiaq Ahmed <mdistiaqahmed0@gmail.com>
2024-12-24 18:35:52 -0600

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,27 @@ const Hero = () => (
   <div className="font-nunito bg-gradient-to-br from-orange via-offwhite to-white min-h-screen flex flex-col pt-4">
     <Header />
     <div className="flex-1 container mx-auto flex flex-col md:flex-row items-center px-10 pt-10 md:py-0 relative">
-      <div className="md:w-1/2 flex flex-col md:pl-20">
-        <h2 className="font-charm text-3xl md:text-8xl font-extrabold text-navy">
-          I am Istiaq Ahmed!
-        </h2>
-      </div>
+      <div className="flex flex-row">
+        <div className="mt-20 md:w-1/2 flex md:pl-20">
+          <h2 className="font-charm text-3xl md:text-8xl font-extrabold text-navy">
+            I am Istiaq Ahmed!
+          </h2>
+        </div>
 
-      <div className="mt-4 md:mt-0 md:w-1/2 flex justify-center md:justify-end">
-        <img
-          src={Istiaq}
-          alt="Portrait of Istiaq"
-          className="w-2/3 md:w-screen h-auto"
-          style={{
-            maskImage:
-              "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%)",
-            WebkitMaskImage:
-              "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 20%)",
-            objectFit: "cover",
-          }}
-        />
+        <div className=" md:mt-0 md:w-1/2 flex justify-center md:justify-end">
+          <img
+            src={Istiaq}
+            alt="Portrait of Istiaq"
+            className="w-2/3 md:w-screen h-auto"
+            style={{
+              maskImage:
+                "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 10%)",
+              WebkitMaskImage:
+                "linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 20%)",
+              objectFit: "cover",
+            }}
+          />
+        </div>
       </div>
 
       <div className="hidden md:w-1/3 md:flex flex-col items-end md:pr-24 mt-4 md:pt-60 ">
